0af0bbe14774a4a72dcdaf8a24ab9e4066d94290
saci zakaria <sacideveloppeur@gmail.com>
2021-10-30 18:00:57 +0200

    class Meal

---
+++ lib/models/meal.dart
@@ -0,0 +1,36 @@
+enum Complexity {
+  Simple,
+  Challenging,
+  Hard,
+}
+enum Affordability {
+  Affordable,
+  Pricey,
+  Luxurious,
+}
+
+class Meal {
+  final String id;
+  final List<String> categories;
+  final String imageUrl;
+  final int duration;
+  final Complexity complexity;
+  final Affordability affordability;
+  final bool isGlutenFree;
+  final bool isLactoseFree;
+  final bool isVegan;
+  final bool isVegetarian;
+
+  Meal({
+    this.id,
+    this.categories,
+    this.imageUrl,
+    this.duration,
+    this.complexity,
+    this.affordability,
+    this.isGlutenFree,
+    this.isLactoseFree,
+    this.isVegan,
+    this.isVegetarian,
+  });
+}
