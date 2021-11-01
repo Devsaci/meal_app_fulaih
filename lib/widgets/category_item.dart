fa481758f1667d7c0810bc5b7070296da99c452a
saci zakaria <sacideveloppeur@gmail.com>
2021-11-01 09:40:25 +0100

    const Categoryitem

--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/cupertino.dart';
 
 class Categoryitem extends StatelessWidget {
-  const Categoryitem({Key key}) : super(key: key);
+  const Categoryitem({Key key, this.id, this.title, this.color})
+      : super(key: key);
+  final String id;
+  final String title;
+  final Color color;
 
   @override
   Widget build(BuildContext context) {
