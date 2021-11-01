7b5cfaa55be4c4fc397a395e33f1a33c76354ca7
saci zakaria <sacideveloppeur@gmail.com>
2021-11-01 10:24:04 +0100

    gradient

--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -6,19 +6,27 @@ class Categoryitem extends StatelessWidget {
   final String id;
   final String title;
   final Color color;
+
   // ignore: prefer_const_constructors_in_immutables
   Categoryitem(this.id, this.title, this.color, {Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: (){},
+      onTap: () {},
       child: Container(
         padding: EdgeInsets.all(15),
         child: Text(title),
         decoration: BoxDecoration(
-          color: color,
-          borderRadius: BorderRadius.circular(20)
+            gradient: LinearGradient(
+              colors: [
+                color.withOpacity(0.2),
+                color
+              ],
+              begin: Alignment.topLeft,
+              end:Alignment.bottomRight,),
+            color: color,
+            borderRadius: BorderRadius.circular(20)
 
         ),
 
