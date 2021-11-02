4911e4793361a0a63446503ed6239ca5767ef5e5
saci zakaria <sacideveloppeur@gmail.com>
2021-11-02 18:08:00 +0100

    selectMeal

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
 class MealItem extends StatelessWidget {
-  const MealItem({Key key});
+   MealItem({Key key});
+
+   void selectMeal() {
+   }
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: (){
-
-      },
+      onTap: selectMeal,
     );
   }
 }
