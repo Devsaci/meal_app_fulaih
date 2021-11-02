4cd500a2bef56b83ffcce107f074da52caf8095c
saci zakaria <sacideveloppeur@gmail.com>
2021-11-02 17:30:28 +0100

    InkWell

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -5,6 +5,10 @@ class MealItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return InkWell(
+      onTap: (){
+
+      },
+    );
   }
 }
