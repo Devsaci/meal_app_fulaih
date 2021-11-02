cea785f511bf2810fcb1277fcfc711909f9d70dd
saci zakaria <sacideveloppeur@gmail.com>
2021-11-02 13:03:55 +0100

    ListView.builder/categoryMeals

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app_fulaih/dummy_data.dart';
 
 class CategoryMealsScreen extends StatefulWidget {
   CategoryMealsScreen({Key key});
@@ -13,16 +14,18 @@ class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
   @override
   Widget build(BuildContext context) {
     final routeArg =
-        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    ModalRoute
+        .of(context)
+        .settings
+        .arguments as Map<String, String>;
     final categoryId = routeArg['id'];
     final categorytitle = routeArg['title'];
+    final categoryMeals = DUMMY_MEALS.where((element) => false).toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(categorytitle),
       ),
-      body: Center(
-        child: Text(categorytitle),
-      ),
+      body: ListView.builder(itemBuilder: (ctx, index){}, ),
     );
   }
 }
