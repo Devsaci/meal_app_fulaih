0e71508a9678b1368f40ad6045f19d4c601336d0
saci zakaria <sacideveloppeur@gmail.com>
2021-11-02 18:21:37 +0100

    Column/Stack/ClipRRect

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
 
 class MealItem extends StatelessWidget {
-   MealItem({Key key});
+  MealItem({Key key});
 
-   void selectMeal() {
-   }
+  void selectMeal() {}
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: selectMeal,
       child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          elevation: 2,
+          margin: EdgeInsets.symmetric(vertical: 15,horizontal: 10),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                ClipRRect(
 
+                )
+
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
