d508c61701f3c121d95e0c1d019bc4dd9c8e260d
saci zakaria <sacideveloppeur@gmail.com>
2021-11-04 19:13:16 +0100

    Ingredients

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -24,6 +24,10 @@ class MealDetailScreen extends StatelessWidget {
               fit: BoxFit.cover,
             ),
           ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10),
+            child: Text("Ingredients",style: Theme.of(context).textTheme.subtitle1,),
+          )
         ],
       ),
     );
