1f239eb2c0e138c38b9558e856d7a350e47d7a44
saci zakaria <sacideveloppeur@gmail.com>
2021-11-04 19:44:25 +0100

    Ingredients/Theme

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -26,8 +26,29 @@ class MealDetailScreen extends StatelessWidget {
           ),
           Container(
             margin: EdgeInsets.symmetric(vertical: 10),
-            child: Text("Ingredients",style: Theme.of(context).textTheme.subtitle1,),
-          )
+            child: Text(
+              "Ingredients",
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white38,
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(10),
+            height: 200,
+            width: 300,
+            child: ListView.builder(
+              itemBuilder: (ctx, index) => Card(
+                color: Theme.of(context).accentColor,
+                child: Text(selectedMeal.ingredients[index]),
+              ),
+              itemCount: selectedMeal.ingredients.length,
+            ),
+          ),
         ],
       ),
     );
