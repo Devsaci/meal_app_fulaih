216aaab5fefb0b0307f5837f331e2d5644fe9d1f
saci zakaria <sacideveloppeur@gmail.com>
2021-11-05 12:54:05 +0100

    SingleChildScrollView

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -39,41 +39,43 @@ class MealDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(selectedMeal.title),
       ),
-      body: Column(
-        children: [
-          Container(
-            height: 300,
-            width: double.infinity,
-            child: Image.network(
-              selectedMeal.imageUrl,
-              fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child:  Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                selectedMeal.imageUrl,
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          buildSectionTitle(context, 'Ingredients'),
-          buildContainer(
-            ListView.builder(
-              itemBuilder: (ctx, index) => Card(
-                  color: Theme.of(context).accentColor,
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                    child: Text(selectedMeal.ingredients[index]),
-                  )),
-              itemCount: selectedMeal.ingredients.length,
+            buildSectionTitle(context, 'Ingredients'),
+            buildContainer(
+              ListView.builder(
+                itemBuilder: (ctx, index) => Card(
+                    color: Theme.of(context).accentColor,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                      child: Text(selectedMeal.ingredients[index]),
+                    )),
+                itemCount: selectedMeal.ingredients.length,
+              ),
             ),
-          ),
-          buildSectionTitle(context, 'Steps'),
-          buildContainer(
-            ListView.builder(
-              itemBuilder: (ctx, index) => Card(
-                  color: Theme.of(context).accentColor,
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                    child: Text(selectedMeal.steps[index]),
-                  )),
-              itemCount: selectedMeal.steps.length,
+            buildSectionTitle(context, 'Steps'),
+            buildContainer(
+              ListView.builder(
+                itemBuilder: (ctx, index) => Card(
+                    color: Theme.of(context).accentColor,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                      child: Text(selectedMeal.steps[index]),
+                    )),
+                itemCount: selectedMeal.steps.length,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
