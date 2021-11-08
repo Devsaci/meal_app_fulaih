bf37be1d495e6050e838df44b1efbae13f777a9f
saci zakaria <sacideveloppeur@gmail.com>
2021-11-08 16:45:33 +0100

    134. Meal App Part 13 StatefulWidget

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,21 +1,25 @@
-
-
-
 import 'package:flutter/material.dart';
 import 'package:meal_app_fulaih/widgets/main_drawer.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   const FiltersScreen({Key key}) : super(key: key);
   static const routeName = '/filters';
+
+  @override
+  State<FiltersScreen> createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Your Filters"),),
+      appBar: AppBar(
+        title: Text("Your Filters"),
+      ),
       body: Center(
         child: Text("FiltersScreen"),
       ),
       drawer: MainDrawer(),
-
     );
   }
 }
