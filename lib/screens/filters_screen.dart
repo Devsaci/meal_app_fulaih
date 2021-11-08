08b49eca83fb4cbf66a66db86646d5397de9c065
saci zakaria <sacideveloppeur@gmail.com>
2021-11-08 09:25:26 +0100

    133. Meal App Part 12

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,17 +1,21 @@
 
 
 
-import 'dart:html';
-
 import 'package:flutter/material.dart';
+import 'package:meal_app_fulaih/widgets/main_drawer.dart';
 
 class FiltersScreen extends StatelessWidget {
   const FiltersScreen({Key key}) : super(key: key);
   static const routeName = '/filters';
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("FiltersScreen"),
+    return Scaffold(
+      appBar: AppBar(title: Text("Your Filters"),),
+      body: Center(
+        child: Text("FiltersScreen"),
+      ),
+      drawer: MainDrawer(),
+
     );
   }
 }
